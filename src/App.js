f421b4cca9bef7de0090126f291ce8c7eb057f00
TonyOuma <kyrietony@gmail.com>
2020-10-07 11:19:49 +0300

    feat: link components in the main app component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Movies from './components/Movies';
+import MovieDetails from './components/MovieDetails';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+    <BrowserRouter>
+      <Route path="/" exact component={Movies} />
+      <Route path="/MovieDetails/:id" component={MovieDetails} />
+    </BrowserRouter>
     </div>
   );
 }
